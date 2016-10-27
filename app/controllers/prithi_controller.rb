e8dd5596a2438128eeeffa98be7595e648bcbdee
pprithip <pprithip@asb9838nu-e05.csil.sfu.ca>
2016-10-26 20:06:00 -0700

    homebutton

--- app/controllers/prithi_controller.rb
+++ app/controllers/prithi_controller.rb
@@ -1,5 +1,5 @@
 class PrithiController < ApplicationController
   def index
-    
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
-  get '/prithi' => "prithi#index"
 
+  get '/prithi' => "prithi#index"
+get '/prithi/index' => "prithi#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
