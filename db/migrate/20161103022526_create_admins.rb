6615915085314cba212f0e05f456f22865ba4ad7
Emad Rahman <emad@Emads-MacBook-Pro.local>
2016-11-02 20:20:29 -0700

    worksinlocalbutnotlive

--- db/migrate/20161103022526_create_admins.rb
+++ db/migrate/20161103022526_create_admins.rb
@@ -1,6 +1,6 @@
 class CreateAdmins < ActiveRecord::Migration
   def change
-    # drop_table :admins
+    drop_table :admins
     create_table :admins do |t|
       t.string :email
       t.string :password
