cfbf3f3acb379b2323b50e0920ba845dd21b50b1
Prithi <pprithip@sfu.ca>
2016-11-06 07:34:52 +0000

    New Supervisor working and showing in database

--- app/controllers/supervisors_controller.rb
+++ app/controllers/supervisors_controller.rb
@@ -3,4 +3,13 @@ class SupervisorsController < ApplicationController
     def new
         @supervisor = Supervisor.new
     end
+    
+    def create
+        @supervisor = Supervisor.new(params.require(:supervisor).permit(:name,:age))
+        if @supervisor.save
+            redirect_to(:action => "index")
+        else
+            redirect_to("/")
+        end
+    end
 end
