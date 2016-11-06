c9ccfb601686eb2a31b0a5157c5b451f8539cf46
Akshay <akshayn1388@gmail.com>
2016-11-06 10:22:56 +0000

    Before pulling Prithi SingUpp working

--- app/controllers/session_admnins_controller.rb
+++ app/controllers/session_admnins_controller.rb
@@ -4,14 +4,14 @@ class SessionAdmninsController < ApplicationController
     end 
     
     def create
-     @sign_upp = Sign_upp.find_by_email(params[:session_admnin][:email])
-     
-	if @sign_upp && @sign_upp.authenticate(params[:session_admnin][:password])
-    session_admnin[:sign_upp_id] = @sign_upp.id
-    redirect_to '/'
-    else
-    redirect_to 'login'
-    end 
+        @sign_upp = SignUpp.find_by_email(params[:session][:email])
+         
+    	if @sign_upp && @sign_upp.authenticate(params[:session_admnin][:password])
+            session[:sign_upp_id] = @sign_upp.id
+            redirect_to '/'
+        else
+            redirect_to 'login'
+        end 
     end
     
     def destroy 
@@ -22,22 +22,3 @@ class SessionAdmninsController < ApplicationController
 end 
     
     
-class SessionsController < ApplicationController
-  def new 
-  end 
-  def create
-  @user = 								User.find_by_email(params[:session]	[:email])
-	  if @user && @user.authenticate(params[:session][:password])
-    session[:user_id] = @user.id
-    redirect_to '/'
-  else
-    redirect_to 'login'
-  end 
-end
- def destroy 
-   session[:sign_upp_id] = nil
-   redirect_to '/'
- end 
-  
-end
-
--- app/controllers/sign_upps_controller.rb
+++ app/controllers/sign_upps_controller.rb
@@ -6,7 +6,7 @@ class SignUppsController < ApplicationController
   def create 
     @sign_upp = SignUpp.new(params.require(:sign_upp).permit(:first_name, :last_name, :email, :password)) 
     if @sign_upp.save 
-     # session[:sign_upp_id] = @sign_upp.id 
+      session[:sign_upp_id] = @sign_upp.id 
       redirect_to(:action => "index")
     else 
       redirect_to ("/sign_upps/new")
