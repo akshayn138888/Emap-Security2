101b9729b3485bde4b480b113f9e4351b8e5f5c2
Prithi <pprithip@sfu.ca>
2016-11-07 12:33:18 +0000

    .yml working good. run heroku run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20161106154520) do
     t.datetime "updated_at"
   end
 
+  create_table "main", id: false, force: :cascade do |t|
+    t.string "name", limit: 50
+  end
+
   create_table "services", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
