64741bbad7bd01ff1afb90bae21691741d6792d1
Akshay <akshayn1388@gmail.com>
2016-11-08 03:18:37 +0000

    database and table values validation

--- app/models/sign_upp.rb
+++ app/models/sign_upp.rb
@@ -1,10 +1,13 @@
 class SignUpp < ActiveRecord::Base
-    
-    
-    
-    
     has_secure_password
-    has_many :sign_upp_supervisor
+    validates :first_name, :last_name, :email, :password, presence: true
+    validates :first_name, :last_name, length: { minimum: 2 }
+    
+    validates :password, length: { in: 6..20 }
+  
+    validates :email, uniqueness: true
+
+    has_many :sign_upp_supervisors
     validates_associated :sign_upp_supervisor
     
 end
--- app/models/sign_upp_supervisor.rb
+++ app/models/sign_upp_supervisor.rb
@@ -1,5 +1,12 @@
 class SignUppSupervisor < ActiveRecord::Base
     has_secure_password
+    validates :first_name, :last_name, :email, :password, presence: true
+    validates :first_name, :last_name, length: { minimum: 2 }
+    
+    validates :password, length: { in: 6..20 }
+  
+    validates :email, uniqueness: true
+
     belongs_to :sign_upp
     has_many :sign_upp_security_guards 
     validates_associated :sign_upp 
